ddce77bdd0427192a07ff9db6ec5e79534f170b8
Israel Omotayo <bossbabatee@gmail.com>
2021-09-10 13:01:39 +0100

    Added profiles

--- index.py
+++ index.py
@@ -5,6 +5,32 @@ app = Flask(__name__)
 products = []
 name = 'Tola'
 email = 'tola@pediforte.com'
+users = [
+    {
+        'name': 'Carlos Denver',
+        'age': 54,
+        'nationality': 'Mexican',
+        'email': 'carlos@pediforte.com',
+        'id': 0
+    },
+    {
+        'name': 'Messi Cross',
+        'age': 24,
+        'nationality': 'Spanish',
+        'email': 'messi@pediforte.com',
+        'id': 1
+    }
+]
+
+
+@app.route('/test')
+def test_page():
+    email = 'tola@pediforte.com'
+    fruits = ['Apple', 'Mango', 'Orange', 'Pineapple']
+    show_users = 'yes'
+    return render_template('test.html', em=email, friuts=fruits,
+                           profiles=users, show_users=show_users)
+
 
 @app.route('/')
 def homepage():
@@ -18,7 +44,7 @@ def about_page():
 
 @app.route('/products/')
 def product_page():
-    return render_template('products.html', products=products)
+    return render_template('all_products.html', products=products)
 
 
 @app.route('/add-product', methods=['POST'])
@@ -27,7 +53,7 @@ def add_product():
     price = request.form['price']
     products.append(
         {'title': title, 'price': price,
-         'image': 'https://picsum.photos/seed/'+title+'/200/200'}
+         'image': 'https://picsum.photos/seed/' + title + '/200/200'}
     )
     return redirect(url_for('product_page'))
 
@@ -35,3 +61,10 @@ def add_product():
 @app.route('/add-product-page')
 def add_product_page():
     return render_template('add-product.html')
+
+
+@app.route('/profile/<uid>')
+def get_user(uid):
+    uid = int(uid)
+    profile = users[uid]
+    return render_template('profile.html', profile=profile)
